ddfbe1f33aec973089035f7b8ef1bed1e929ffbc
juhyeong <ghd5427@naver.com>
2024-04-01 20:20:55 +0900

    - 영속성 컨텍스트2 - 엔티티 테이블 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -40,22 +40,46 @@ public class JpaMain {
 //            for (Member member: result) {
 //                System.out.println("member.getName() = " + member.getName());
 //            }
+//
 
-            //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
-            //영속 -persist context
-            System.out.println("==== BEFORE ====");
-            em.persist(member);
+//            //비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            //영속 -persist context
+//            System.out.println("==== BEFORE ====");
+//            em.persist(member); //1차캐시 영속 컨텍스트에 저장
 //            em.detach(member);
 
-            System.out.println("==== AFTER ====");
 
+
+//            System.out.println("==== AFTER ====");
+//
+//            Member findMember1 = em.find(Member.class, 101L); //1차 캐시에서 찾음 - 없으면 DB에서 찾음 찾고 잇으면 다시 1차캐시에 올림
+//            Member findMember2 = em.find(Member.class, 101L); //1차 캐시에서 찾음 - 없으면 DB에서 찾음 찾고 잇으면 다시 1차캐시에 올림
+//
+//            System.out.println("result = " + (findMember1==findMember2));
+
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
             //삭제
 //            em.remove(findMember);
 
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//            em.persist(member1);
+//            em.persist(member2);
+
+//            Member member = em.find(Member.class, 150L);
+//            member.setName("zzzzz"); //엔티티 변경 commit되는 시점에 자동으로 update
+
+//            Member member = new Member(200L, "member200");
+//            em.persist(member);
+
+            em.flush(); //데이터베이스에 쿼리가 즉시 나감
+
+            System.out.println("==============================");
             tx.commit();
         }catch (Exception e) {
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,30 +1,34 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
+@SequenceGenerator(name="member_seq_generator", sequenceName = "member_seq")
+//@Table(name="MBR")
 public class Member {
-    @Id
-    private Long id;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+//    private Long id;
+//    @Column(name = "name")
+//    private String username;
 
-    private String name;
+//    private Integer age;
+//    @Enumerated(EnumType.STRING) //필수로 enum에 저장된 String으로 표시
+//    private RoleType roleType;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    private LocalDate testLocalDate;
+//    private LocalDateTime testLocalDateTime;
+//
+//    @Lob //문자면 clob
+//    private String description; //큰 컨텐트를 넣고싶으면 lob
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
